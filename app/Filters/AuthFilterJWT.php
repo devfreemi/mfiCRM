ae38bfaf99e093148db621216296b0264d913815
devfreemi <devfreemi@gmail.com>
2024-06-24 23:43:24 +0530

    JWT

--- app/Filters/AuthFilterJWT.php
+++ app/Filters/AuthFilterJWT.php
@@ -5,6 +5,8 @@ namespace App\Filters;
 use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class AuthFilterJWT implements FilterInterface
 {
@@ -26,6 +28,34 @@ class AuthFilterJWT implements FilterInterface
     public function before(RequestInterface $request, $arguments = null)
     {
         //
+        $key = getenv('JWT_SECRET');
+        $header = $request->getHeader("Authorization");
+        $token = null;
+
+        // extract the token from the header
+        if (!empty($header)) {
+            if (preg_match('/Bearer\s(\S+)/', $header, $matches)) {
+                $token = $matches[1];
+            }
+        }
+
+        // check if token is null or empty
+        if (is_null($token) || empty($token)) {
+            $response = service('response');
+            $response->setBody('Access denied');
+            $response->setStatusCode(401);
+            return $response;
+        }
+
+        try {
+            // $decoded = JWT::decode($token, $key, array("HS256"));
+            $decoded = JWT::decode($token, new Key($key, 'HS256'));
+        } catch (Exception $ex) {
+            $response = service('response');
+            $response->setBody('Access denied');
+            $response->setStatusCode(401);
+            return $response;
+        }
     }
 
     /**
